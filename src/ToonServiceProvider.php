a638d47b6195d8ac2a9e8511732f9a26da1cc0f3
Sagar Sunil Bhedodkar <sagarbhedodkar456@gmail.com>
2025-11-02 12:44:16 +0530

    Fix namespace typo in ToonServiceProvider

--- src/ToonServiceProvider.php
+++ src/ToonServiceProvider.php
@@ -3,15 +3,18 @@
 namespace Sbsaga\Toon;
 
 use Illuminate\Support\ServiceProvider;
+use Sbsaga\Toon\Converters\ToonConverter;
 
 class ToonServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        // Publish config file
         $this->publishes([
             __DIR__ . '/../config/toon.php' => config_path('toon.php'),
         ], 'config');
 
+        // Register artisan commands if running in console
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Console\ToonConvertCommand::class,
@@ -21,10 +24,12 @@ class ToonServiceProvider extends ServiceProvider
 
     public function register()
     {
+        // Merge configuration
         $this->mergeConfigFrom(__DIR__ . '/../config/toon.php', 'toon');
 
+        // Bind the main Toon service into the container
         $this->app->singleton('toon', function ($app) {
-            return new Toon(new \Sbsagar\Toon\Converters\ToonConverter(config('toon')));
+            return new Toon(new ToonConverter(config('toon')));
         });
     }
 }
